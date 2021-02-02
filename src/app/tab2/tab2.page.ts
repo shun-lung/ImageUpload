8a97124436026c827d1e91b039cc66b3a3d195e1
eyc4623 <eyc4623@gmail.com>
2021-02-02 22:42:43 +0800

    create first my ionic app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -8,5 +11,38 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+ 
+  public preview:any;
+  public showText = true;
+
+  loadImageFromDevice(event) {
+
+    const file = event.target.files[0];
+  
+    const reader = new FileReader();
+  
+    reader.readAsArrayBuffer(file);
+  
+    reader.onload = () => {
+  
+      // get the blob of the image:
+      let blob: Blob = new Blob([new Uint8Array((reader.result as ArrayBuffer))]);
+      
+      // create blobURL, such that we could use it in an image element:
+      let blobURL: string = URL.createObjectURL(blob);
+
+      // show image 
+      this.preview =  blobURL;
+      this.showText = false;
+      
+  
+    };
+  
+    reader.onerror = (error) => {
+  
+      //handle errors
+  
+    };
+  };
 
 }
